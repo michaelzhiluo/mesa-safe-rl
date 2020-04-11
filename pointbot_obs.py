105652f32e98ff335a2ef545ffedf225b17b4966
Brijen Thananjeyan <bthananjeyan@berkeley.edu>
2020-04-11 03:54:44 -0700

    redundant

--- pointbot_obs.py
+++ pointbot_obs.py
@@ -36,27 +36,6 @@ AIR_RESIST = 0.2
 HARD_MODE = False
 
 
-class Obstacle:
-    def __init__(self, boundsx, boundsy, penalty=100):
-        self.boundsx = boundsx
-        self.boundsy = boundsy
-        self.penalty = 1
-
-
-    def __call__(self, x):
-        return (self.boundsx[0] <= x[0] <= self.boundsx[1] and self.boundsy[0] <= x[1] <= self.boundsy[1]) * self.penalty
-
-class ComplexObstacle(Obstacle):
-
-    def __init__(self, bounds):
-        self.obs = []
-        for boundsx, boundsy in bounds:
-            self.obs.append(Obstacle(boundsx, boundsy))
-
-    def __call__(self, x):
-        return np.max([o(x) for o in self.obs])
-
-
 OBSTACLE = [
         [[-30, -20], [-10, 10]]]
 
