4d8dc7f89d958802f742a624d6c45fef4b965b88
Brijen Thananjeyan <bthananjeyan@berkeley.edu>
2020-04-22 16:09:46 -0700

    run-params

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 # To run channel/tightrope env
-python main.py --cuda --env-name SimplePointBot-v0
+python main.py --env-name simplepointbot0 --cuda --use_recovery --gamma_safe 0.8 --eps_safe 0.1
 
 # To run obstacle env
 python main.py --cuda --env-name SimplePointBot-v1 --gamma_safe 0.9 --num_steps 40000
