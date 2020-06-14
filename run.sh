fb89f23087b1e88a4ac50324aebaf54f80169e9f
Ashwin Balakrishna <abalakrishna@gmail.com>
2020-06-14 01:48:16 -0700

    run.sh update WIP

--- run.sh
+++ run.sh
@@ -1,11 +1,81 @@
-# To run channel/tightrope env
+# --- POINTBOT 0 ENV ---
+# Recovery RL
 python main.py --env-name simplepointbot0 --cuda --use_recovery --gamma_safe 0.8 --eps_safe 0.1 --use_value
-python main.py --env-name simplepointbot0 --cuda --use_recovery --gamma_safe 1 --use_value --pred_time --t_safe 70
 python main.py --env-name simplepointbot0 --cuda --use_recovery --gamma_safe 0.8 --eps_safe 0.2 --use_qvalue
+# Unconstrained
 
-# To run obstacle env
+# Constrained
+
+# Lagrangian
+
+# RCPO
+
+# --- POINTBOT 1 ENV ---
+# Recovery RL
 python main.py --env-name simplepointbot1 --cuda --use_recovery --gamma_safe 0.9 --eps_safe 0.1 --use_value
 python main.py --env-name simplepointbot1 --cuda --use_recovery --gamma_safe 0.75 --eps_safe 0.2 --use_qvalue
+# Unconstrained
 
-# To run maze env
+# Constrained
+
+# Lagrangian
+
+# RCPO
+
+# --- MAZE ENV --- 
+# Recovery RL
 python -m main --cuda --env-name maze --use_recovery --use_value --critic_safe_update_freq 5 --recovery_policy_update_freq 5 --gamma_safe 0.85 --eps_safe 0.05
+# Unconstrained
+
+# Constrained
+
+# Lagrangian
+
+# RCPO
+
+# --- SHELF ENV ---
+# Recovery RL
+
+# Unconstrained
+
+# Constrained
+
+# Lagrangian
+
+# RCPO
+
+# --- DYNAMIC SHELF ENV ---
+
+# Recovery RL
+
+# Unconstrained
+
+# Constrained
+
+# Lagrangian
+
+# RCPO
+
+# --- IMAGE MAZE ENV ---
+
+# Recovery RL
+
+# Unconstrained
+
+# Constrained
+
+# Lagrangian
+
+# RCPO
+
+# --- SHELF ENV ---
+
+# Recovery RL
+
+# Unconstrained
+
+# Constrained
+
+# Lagrangian
+
+# RCPO
