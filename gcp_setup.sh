db75356d743e2b0b3bedeb5e10247f10c9e78513
Brijen Thananjeyan <bthananjeyan@berkeley.edu>
2020-06-15 15:41:52 -0700

    edits

--- gcp_setup.sh
+++ gcp_setup.sh
@@ -3,10 +3,11 @@
 sudo apt update
 sudo apt install -y python3-pip ffmpeg zip unzip libsm6 libxext6 libgl1-mesa-dev libosmesa6-dev libgl1-mesa-glx patchelf
 
-pip3 install numpy scipy gym dotmap matplotlib tqdm opencv-python
+pip3 install numpy scipy gym dotmap matplotlib tqdm opencv-python tensorboardX moviepy
 pip3 install tensorflow-gpu==1.14.0
 pip3 install torch==1.4.0
 pip3 install torchvision==0.5.0
+pip3 install mujoco_py==1.50.1.68
 
 curl https://www.roboti.us/download/mjpro150_linux.zip --output mjpro150_linux.zip
 unzip mjpro150_linux.zip
@@ -14,3 +15,10 @@ mkdir ~/.mujoco
 mv mjpro150 ~/.mujoco/
 rm mjpro150_linux.zip
 cp mjkey.txt ~/.mujoco/
+
+echo 'alias python=python3' >> ~/.bashrc
+echo 'alias pip=pip3' >> ~/.bashrc
+echo 'export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/brijenthananjeyan/.mujoco/mjpro150/bin' >> ~/.bashrc
+. ~/.bashrc
+
+# upload demo data (can't automate this)
