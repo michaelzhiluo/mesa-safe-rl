9fa6f3690dcba05656269bd1c6561856d034adcc
Brijen Thananjeyan <bthananjeyan@berkeley.edu>
2020-06-15 14:49:41 -0700

    gcp-setup

---
+++ gcp_setup.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+sudo apt update
+sudo apt install -y python3-pip ffmpeg zip unzip libsm6 libxext6 libgl1-mesa-dev libosmesa6-dev libgl1-mesa-glx patchelf
+
+pip3 install numpy scipy gym dotmap matplotlib tqdm opencv-python
+pip3 install tensorflow-gpu==1.14.0
+pip3 install torch==1.4.0
+pip3 install torchvision==0.5.0
+
+curl https://www.roboti.us/download/mjpro150_linux.zip --output mjpro150_linux.zip
+unzip mjpro150_linux.zip
+mkdir ~/.mujoco
+mv mjpro150 ~/.mujoco/
+rm mjpro150_linux.zip
