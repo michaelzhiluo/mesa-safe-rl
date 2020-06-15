1be2a517ca6cda13615969c29b2ebf9aface9cb9
Brijen Thananjeyan <bthananjeyan@berkeley.edu>
2020-06-15 14:26:36 -0700

    turn eval off

--- main.py
+++ main.py
@@ -195,7 +195,7 @@ parser.add_argument('--logdir_suffix', default="",
                     help='log directory suffix')
 parser.add_argument('--policy', default="Gaussian",
                     help='Policy Type: Gaussian | Deterministic (default: Gaussian)')
-parser.add_argument('--eval', type=bool, default=True,
+parser.add_argument('--eval', type=bool, default=False,
                     help='Evaluates a policy a policy every 10 episode (default: True)')
 parser.add_argument('--gamma', type=float, default=0.99, metavar='G',
                     help='discount factor for reward (default: 0.99)')
