400ecade4a2bbb170b17b90b93efd06737c239fb
Brijen Thananjeyan <bthananjeyan@berkeley.edu>
2020-06-17 22:54:19 -0700

    ...

--- main.py
+++ main.py
@@ -498,6 +498,6 @@ for i_episode in itertools.count(1):
         print("Test Episodes: {}, Avg. Reward: {}".format(episodes, round(avg_reward, 2)))
         print("----------------------------------------")
 
-        dump_logs(test_rollouts, train_rollouts, logdir)
+    dump_logs(test_rollouts, train_rollouts, logdir)
         
 env.close()
