8d170a731cf67618d60dcb7fb46169f47eeaaa39
Ashwin Balakrishna <abalakrishna@gmail.com>
2020-06-25 16:34:27 -0700

    small logging bug fix

--- main.py
+++ main.py
@@ -86,10 +86,10 @@ def agent_setup(env, logdir, args):
 
 def get_action(state, env, agent, recovery_policy, args, train=True, im_state=None):
     def recovery_thresh(state, action, agent, recovery_policy, args):
+        critic_val = agent.safety_critic.get_value(torchify(state).unsqueeze(0), torchify(action).unsqueeze(0)) # TODO: make sure this is exactly equal to reachability_hor=1
         if not args.use_recovery:
             print("FORWARD: ", critic_val)
             return False
-        critic_val = agent.safety_critic.get_value(torchify(state).unsqueeze(0), torchify(action).unsqueeze(0)) # TODO: make sure this is exactly equal to reachability_hor=1
         if args.reachability_test: # reachability test combined with safety check
             return not recovery_policy.reachability_test(state, action, args.eps_safe)
         if args.lookahead_test:
