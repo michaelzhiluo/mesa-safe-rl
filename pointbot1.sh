7fc6bb30bbe8826e5d74ca9f2e3f8296c4657c16
Brijen Thananjeyan <bthananjeyan@berkeley.edu>
2020-07-03 22:36:03 -0700

    hyperparams

--- pointbot1.sh
+++ pointbot1.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-for i in {2..3}
+for i in {1..3}
 do
 	echo "Recovery Run $i"
-	python main.py --env-name simplepointbot1 --cuda --use_recovery --gamma_safe 0.8 --eps_safe 0.1 --use_qvalue --logdir pointbot1 --logdir_suffix recovery_fast --num_eps 300 --seed $i --tau_safe 0.001
+	python main.py --env-name simplepointbot1 --cuda --use_recovery --gamma_safe 0.8 --eps_safe 0.1 --use_qvalue --logdir pointbot1 --logdir_suffix recovery_fast --num_eps 300 --seed $i --tau_safe 0.001 --pos_fraction 0.1
 done
 
 # for i in {1..3}
