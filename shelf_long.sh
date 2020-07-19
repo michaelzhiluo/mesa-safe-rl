d9472579489e7ca10569c2cf615b935e5514d99d
Ashwin Balakrishna <abalakrishna@gmail.com>
2020-07-18 20:48:08 -0700

    fixed shell script

--- shelf_long.sh
+++ shelf_long.sh
@@ -3,7 +3,7 @@
 # Recovery RL PETS Recovery
 # for i in {1..3}
 # do
-# 	python -m main --cuda --env-name shelf_long_env --task_demos --alpha 0.05 --tau 0.0002 --replay_size 100000 --use_recovery --recovery_policy_update_freq 20 --gamma_safe 0.85 --eps_safe 0.25 --use_qvalue --num_eps 4000 --logdir shelf_long_env --logdir_suffix recovery_ddpg_0.85_0.25 --pos_fraction 0.3 --seed $i
+# 	python -m main --cuda --env-name shelf_long_env --task_demos --alpha 0.05 --tau 0.0002 --replay_size 100000 --use_recovery --recovery_policy_update_freq 20 --gamma_safe 0.85 --eps_safe 0.25 --use_qvalue --num_eps 4000 --logdir shelf_long_env --logdir_suffix recovery_0.85_0.25 --pos_fraction 0.3 --seed $i
 # done
 
 # # Recovery RL DDPG Recovey
@@ -16,19 +16,19 @@
 # Unconstrained
 # for i in {1..3}
 # do
-# 	python -m main --cuda --env-name shelf_long_env --task_demos --alpha 0.05 --tau 0.0002 --replay_size 100000 --use_recovery --critic_safe_update_freq 20 --recovery_policy_update_freq 20 --gamma_safe 0.85 --eps_safe 0.25 --use_qvalue --ddpg_recovery --num_eps 4000 --logdir shelf_long_env --logdir_suffix recovery_ddpg_0.85_0.35 --pos_fraction 0.3 --seed $i
+# 	python -m main --cuda --env-name shelf_long_env --task_demos --alpha 0.05 --tau 0.0002 --replay_size 100000 --num_eps 4000 --logdir shelf_long_env --logdir_suffix vanilla --pos_fraction 0.3 --seed $i
 # done
 
 # Reward Penalty 10
 # for i in {1..3}
 # do
-# 	python -m main --cuda --env-name shelf_long_env --task_demos --alpha 0.05 --tau 0.0002 --replay_size 100000 --num_eps 4000 --constraint_reward_penalty 10 -logdir shelf_long_env --logdir_suffix reward_10 --pos_fraction 0.3 --seed $i
+# 	python -m main --cuda --env-name shelf_long_env --task_demos --alpha 0.05 --tau 0.0002 --replay_size 100000 --num_eps 4000 --constraint_reward_penalty 10 --logdir shelf_long_env --logdir_suffix reward_10 --pos_fraction 0.3 --seed $i
 # done
 
 # SAC Lagrangian Nu = 10 (Update nu)
 # for i in {1..3}
 # do
-# 	python -m main --cuda --env-name shelf_long_env --task_demos --alpha 0.05 --tau 0.0002 --replay_size 100000 --use_recovery --critic_safe_update_freq 20 --recovery_policy_update_freq 20 --gamma_safe 0.75 --eps_safe 0.25 --use_qvalue --ddpg_recovery --num_eps 4000 --logdir shelf_long_env --logdir_suffix recovery_ddpg_0.75_0.25 --pos_fraction 0.3 --seed $i
+# 	python -m main --cuda --env-name shelf_long_env --task_demos --alpha 0.05 --tau 0.0002 --replay_size 100000 --gamma_safe 0.75 --eps_safe 0.25 --use_qvalue --DGD_constraints --nu 10 --num_eps 4000 --logdir shelf_long_env --logdir_suffix nu_10 --pos_fraction 0.3 --seed $i
 # done
 
 # SAC Lagrangian RSPO (Update nu) # Start at 2x best nu and decay
