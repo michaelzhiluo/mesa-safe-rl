1e2cc6ad0063b29f6d6ad0825ee02de55041344c
Brijen Thananjeyan <bthananjeyan@berkeley.edu>
2020-08-08 23:35:02 -0700

    rendering

--- env/simplepointbot0.py
+++ env/simplepointbot0.py
@@ -16,6 +16,12 @@ from gym.spaces import Box
 
 from obstacle import Obstacle, ComplexObstacle
 
+import matplotlib.pyplot as plt
+import matplotlib.patches as patches
+import numpy as np
+import io
+import cv2
+
 """
 Constants associated with the PointBot env.
 """
@@ -93,6 +99,7 @@ class SimplePointBot(Env, utils.EzPickle):
                 "constraint": self.obstacle(next_state),
                 "reward": cur_cost,
                 "state": old_state,
+                # "im": np.array(render(old_state)),
                 "next_state": next_state,
                 "action": a}
 
@@ -190,6 +197,90 @@ def teacher_action(state, goal=GOAL_STATE):
     disp[disp > MAX_FORCE] = MAX_FORCE
     disp[disp < -MAX_FORCE] = -MAX_FORCE
     return disp
+
+
+def render(loc):
+    def get_img_from_fig(fig, dpi=180):
+        buf = io.BytesIO()
+        fig.savefig(buf, format="png", dpi=dpi)
+        buf.seek(0)
+        img_arr = np.frombuffer(buf.getvalue(), dtype=np.uint8)
+        buf.close()
+        img = cv2.imdecode(img_arr, 1)
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+
+        return img
+    data_set = np.array([
+        [.9, .9], [.85, 2.1], [1.2, 1.], [2.1, .95], [3., 1.1],
+        [3.9, .7], [4., 1.4], [4.2, 1.8], [2., 2.3], [3., 2.3],
+        [1.5, 1.8], [2., 1.5], [2.2, 2.], [2.6, 1.7], [2.7, 1.85]
+    ])
+    categories = np.array([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1])
+    color1 = (0.69411766529083252, 0.3490196168422699, 0.15686275064945221, 1.0)
+    color2 = (0.65098041296005249, 0.80784314870834351, 0.89019608497619629, 1.0)
+    colormap = np.array([color1, color2])
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    # ax.scatter(
+    #     x=[data_set[:, 0]],
+    #     y=[data_set[:, 1]],
+    #     c=colormap[categories],
+    #     marker='o',
+    #     alpha=0.9
+    # )
+
+    margin = .1
+    min_f0, max_f0 = -70, 20
+    min_f1, max_f1 = 5, 10
+    width = max_f0 - min_f0
+    height = max_f1 - min_f1
+
+    ax.add_patch(
+        patches.Rectangle(
+            xy=(min_f0, min_f1),  # point of origin.
+            width=width,
+            height=height,
+            linewidth=1,
+            color='red',
+            fill=True
+        )
+    )
+
+
+    margin = .1
+    min_f0, max_f0 = -70, 20
+    min_f1, max_f1 = -10, -5
+    width = max_f0 - min_f0
+    height = max_f1 - min_f1
+
+    ax.add_patch(
+        patches.Rectangle(
+            xy=(min_f0, min_f1),  # point of origin.
+            width=width,
+            height=height,
+            linewidth=1,
+            color='red',
+            fill=True
+        )
+    )
+
+    circle = plt.Circle(loc, radius=1, color='green')
+    ax.add_patch(circle)
+    circle = plt.Circle((-50, 0), radius=1)
+    ax.add_patch(circle)
+    circle = plt.Circle((0, 0), radius=1)
+    ax.add_patch(circle)
+    label = ax.annotate("start", xy=(-50, 3), fontsize=10, ha="center")
+    label = ax.annotate("goal", xy=(0, 3), fontsize=10, ha="center")
+
+
+    plt.xlim(-60, 10)
+    plt.ylim(-10, 10)
+
+    ax.set_aspect('equal')
+    ax.autoscale_view()
+    # plt.savefig("pointbot0_cartoon.png")
+    return get_img_from_fig(fig)
 
 class SimplePointBotTeacher(object):
 
--- env/simplepointbot1.py
+++ env/simplepointbot1.py
@@ -15,7 +15,11 @@ from gym import utils
 from gym.spaces import Box
 
 from obstacle import Obstacle, ComplexObstacle
-
+import matplotlib.pyplot as plt
+import matplotlib.patches as patches
+import numpy as np
+import io
+import cv2
 
 """
 Constants associated with the PointBot env.
@@ -91,6 +95,7 @@ class SimplePointBot(Env, utils.EzPickle):
         return self.state, cur_cost, self.done, {
                 "constraint": self.obstacle(next_state),
                 "reward": cur_cost,
+                # "im": np.array(render(old_state)),
                 "state": old_state,
                 "next_state": next_state,
                 "action": a}
@@ -222,6 +227,75 @@ def get_random_transitions(num_transitions, task_demos=False, save_rollouts=Fals
         return rollouts
     else:
         return transitions
+
+
+
+def render(loc):
+    def get_img_from_fig(fig, dpi=180):
+        buf = io.BytesIO()
+        fig.savefig(buf, format="png", dpi=dpi)
+        buf.seek(0)
+        img_arr = np.frombuffer(buf.getvalue(), dtype=np.uint8)
+        buf.close()
+        img = cv2.imdecode(img_arr, 1)
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+
+        return img
+    data_set = np.array([
+        [.9, .9], [.85, 2.1], [1.2, 1.], [2.1, .95], [3., 1.1],
+        [3.9, .7], [4., 1.4], [4.2, 1.8], [2., 2.3], [3., 2.3],
+        [1.5, 1.8], [2., 1.5], [2.2, 2.], [2.6, 1.7], [2.7, 1.85]
+    ])
+    categories = np.array([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1])
+    color1 = (0.69411766529083252, 0.3490196168422699, 0.15686275064945221, 1.0)
+    color2 = (0.65098041296005249, 0.80784314870834351, 0.89019608497619629, 1.0)
+    colormap = np.array([color1, color2])
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    # ax.scatter(
+    #     x=[data_set[:, 0]],
+    #     y=[data_set[:, 1]],
+    #     c=colormap[categories],
+    #     marker='o',
+    #     alpha=0.9
+    # )
+
+    margin = .1
+    min_f0, max_f0 = -30, -20
+    min_f1, max_f1 = -7.5, 7.5
+    width = max_f0 - min_f0
+    height = max_f1 - min_f1
+
+    ax.add_patch(
+        patches.Rectangle(
+            xy=(min_f0, min_f1),  # point of origin.
+            width=width,
+            height=height,
+            linewidth=1,
+            color='red',
+            fill=True
+        )
+    )
+
+
+    circle = plt.Circle(loc, radius=1, color='green')
+    ax.add_patch(circle)
+    circle = plt.Circle((-50, 0), radius=1)
+    ax.add_patch(circle)
+    circle = plt.Circle((0, 0), radius=1)
+    ax.add_patch(circle)
+    label = ax.annotate("start", xy=(-50, 3), fontsize=10, ha="center")
+    label = ax.annotate("goal", xy=(0, 3), fontsize=10, ha="center")
+
+
+    plt.xlim(-60, 10)
+    plt.ylim(-30, 30)
+
+    ax.set_aspect('equal')
+    ax.autoscale_view()
+    # plt.savefig("pointbot0_cartoon.png")
+    return get_img_from_fig(fig)
+
 
 def safe_action(state, goal=GOAL_STATE):
     dx = dy = 0
